f398e7e2417e318f024d80bb04adf074ecc1cbf9
paowpau <paulinadobek@icloud.com>
2025-01-31 11:39:43 +0100

    coimpleted 2

---
+++ script.js
@@ -0,0 +1,10 @@
+const projectCards = document.querySelectorAll('.project-card');
+
+projectCards.forEach(card => {
+    card.addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+});
+
+//Source: MDN Web Docs - querySelectorAll
+// Link: https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
