4c7b5452393678c9f3599d906129fe0e1af9e7b0
paowpau <paulinadobek@icloud.com>
2025-02-06 17:31:47 +0100

    assigment 3 and 4

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ projectCards.forEach(card => {
 
 //Source: MDN Web Docs - querySelectorAll
 // Link: https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+
+let form = document.getElementById('contact-form');
+let confirmationMessage = document.getElementById('confirmation-message');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    form.style.display = 'none';
+    confirmationMessage.style.display = 'block';
+});
